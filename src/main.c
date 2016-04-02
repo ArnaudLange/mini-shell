cd7cd67617a32b3a9dc797ca28743b2802573ecb
Antoine Sauray <sauray.antoine@gmail.com>
2016-04-02 12:29:56 +0200

    cleaned project architecture

--- src/main.c
+++ src/main.c
@@ -46,27 +46,25 @@ int main(int argc, char* argv[]){
                 // lecture ligne par ligne jusqu'à fin du message entré dans stdin
                 while ((read = getline(&line, &size, stdin)) != -1) {                   
                         // suppression des retour chariot
-                        if (chariot = strchr(line,'\n')){  
-                                printf("1\n");                                                     
+                        if (chariot = strchr(line,'\n')){                                                     
                                 chariot = NULL;
                                 //le pointeur de \n devient pointeur null
                         }                                                                                       
                         //si la ligne est vide
                         if (!strcmp(line,"")){
                                 // on passe a la prochaine   
-                                printf("2\n"); 
                                 return EXIT_SUCCESS;                                                                                                
                         }
                         else{
-                                printf("3\n"); 
-                                
+
+                                /*
                                 ParsedCommand* c = parseCommand(line);
                                 printName(c);
                                 printParameters(c);
                                 printOptions(c);
                                 free(c);
                                 execute(line,line);
-                                
+                                */
                                 
                                 /*ParsedCommand* c = malloc(sizeof(ParsedCommand));
                                 strncpy(c->name, "cd", NAME_SIZE);

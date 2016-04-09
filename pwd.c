28352351a55e94ec59f28e53055aee9838cc28c0
Arnaud Langelier <arnaud.langelier@etu.univ-nantes.fr>
2016-04-09 19:24:27 +0200

    début de mise en forme pour passage sur develop

--- pwd.c
+++ pwd.c
@@ -1,10 +1,25 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <getopt.h>
-//#include "utils.h"
+/*
+    This file is part of Binsh.
 
-int main(int argc, char *argv[]){
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Binsh.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include "../../include/commands/pwd.h"
+
+
+int pwd_lib(int argc, char *argv[]){
 	
 	// -----------------------------------
     // Declaration tableau  pour les options
---
+++ pwd.h
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+#include "utils.h"

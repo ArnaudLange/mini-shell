9f4f99d248d837b05433789a880c94b4dc6e9db4
Arnaud Langelier <arnaud.langelier@etu.univ-nantes.fr>
2016-04-15 15:07:54 +0200

    réindentation

--- fonction.c
+++ fonction.c
@@ -1,18 +1,18 @@
 /*
-    This file is part of Binsh.
+This file is part of Binsh.
 
-    Foobar is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+Foobar is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Binsh.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with Binsh.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 //** à décommenter et à completer une fois la fonction finies **//
@@ -65,44 +65,44 @@ int fonction_lib(int argc, char *argv[]){
 
         //Boucle pour parcourir les options, exemple avec les options verbose et help
         while (1) {
-        int option_index = 0;
+            int option_index = 0;
 
-        //structure donnant les options gérées par la commande
-            //si l'option peut prendre un argument (ex : --port:8080) à la place de no_argument on mettra required_argument
-        static struct option long_options[] = {
-            {"help",     no_argument,       0, 'h'},
-            {"verbose",  no_argument,       0, 'v'}
-        };
+            //structure donnant les options gérées par la commande
+                //si l'option peut prendre un argument (ex : --port:8080) à la place de no_argument on mettra required_argument
+            static struct option long_options[] = {
+                {"help",     no_argument,       0, 'h'},
+                {"verbose",  no_argument,       0, 'v'}
+            };
 
-        //dans le getopt_long, changer le troisième argument et rajouter les options attendues, ici "hv"
-            //si l'option peut prendre un argument on mettra ":" après la lettre (ex: "hvp:")
-        c = getopt_long(argc, argv, "hv", long_options, &option_index);
+            //dans le getopt_long, changer le troisième argument et rajouter les options attendues, ici "hv"
+                //si l'option peut prendre un argument on mettra ":" après la lettre (ex: "hvp:")
+            c = getopt_long(argc, argv, "hv", long_options, &option_index);
 
-        if (c == -1) break;
+            if (c == -1) break;
 
-        //switch en fonction des options rentrées par l'user
-        switch (c) {
+            //switch en fonction des options rentrées par l'user
+            switch (c) {
 
-         case 'h': 
-           printf("\n-----------------------------------------------------------\n");
-           printf("Usage: fonction [OPTION]... ARG\n");
-           printf("Rename SOURCE to DEST\n\n");
-           printf("    -v, --verbose        explain what is being done\n");
-           printf("\n-----------------------------------------------------------\n\n");
-           exit(0);
-           break;
+                case 'h': 
+                printf("\n-----------------------------------------------------------\n");
+                printf("Usage: fonction [OPTION]... ARG\n");
+                printf("Rename SOURCE to DEST\n\n");
+                printf("    -v, --verbose        explain what is being done\n");
+                printf("\n-----------------------------------------------------------\n\n");
+                exit(0);
+                break;
 
-         case 'v':
-           options[nbOptions] = c; 
-           nbOptions++;
-           break;
+                case 'v':
+                options[nbOptions] = c; 
+                nbOptions++;
+                break;
 
-        //message par défaut quand l'option rentrée n'est pas gérée par la commande
-        default:
-            printf("Try 'fonction --help' for more information.\n");
-            exit(1);
-      }
-    }
+                //message par défaut quand l'option rentrée n'est pas gérée par la commande
+                default:
+                printf("Try 'fonction --help' for more information.\n");
+                exit(1);
+            }
+        }
         //Boucle pour parcourir les arguments qui ne sont pas des options
         for (i=1; i<argc; i++)
         {
@@ -133,8 +133,7 @@ int fonction_lib(int argc, char *argv[]){
     }
     //** A changer **//
     //SI pas d'arguments => on affiche une erreur
-    else
-    {
+    else {
         printf("fonction : invalid number of arguments\n");
     }
 
